eaed5cee5bbbbbd3446808b30f665fd32f6d657b
thienquy05 <117158941+thienquy05@users.noreply.github.com>
2025-07-17 19:13:08 -0400

    Deploy

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ load_dotenv('assets/.env')
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY") # secure session support
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://phish-tracker-demo.netlify.app"}})
 
 # Register blueprint
 app.register_blueprint(main)
